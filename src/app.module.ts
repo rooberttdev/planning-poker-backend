b986d391c06760668ca6aae5a5ba8707d15b10ea
robert.lindoso <robert.lindoso@conecthus.org.br>
2025-07-22 03:30:05 -0400

    fix: module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
 import { PlanningPokerModule } from './planning-poker/planning-poker.module';
 import { RoomsController } from './planning-poker/plannimg-pokert.controller';
-import { PlanningPokerService } from './planning-poker/planning-poker.service';
-import { PlanningPokerGateway } from './planning-poker/planning-poker.gateway';
 
 @Module({
   imports: [PlanningPokerModule],
-  controllers: [RoomsController],
-  providers: [PlanningPokerService, PlanningPokerGateway],
 })
 export class AppModule {}
--- src/planning-poker/planning-poker.module.ts
+++ src/planning-poker/planning-poker.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PlanningPokerService } from './planning-poker.service';
 import { PlanningPokerGateway } from './planning-poker.gateway';
+import { RoomsController } from './plannimg-pokert.controller';
 
 @Module({
   providers: [PlanningPokerService, PlanningPokerGateway],
+  controllers: [RoomsController],
 })
 export class PlanningPokerModule {}
